af3e86990ebcfb86010f80b76ca4812daeccefcb
thisisnaoo <127418109+thisisnaoo@users.noreply.github.com>
2023-04-02 11:04:40 +0000

    Main

--- SQL SCRIPT.sql
+++ SQL SCRIPT.sql
@@ -2,20 +2,20 @@ CREATE TABLE KHOA (
     MaKhoa varchar(10) PRIMARY KEY,
     TenKhoa nvarchar(50));
 INSERT INTO KHOA (MaKhoa, TenKhoa) VALUES
-    ('SOM', 'Quản trị'),
-    ('IBM', 'Kinh doanh quốc tế - Marketing'),
-    ('SOF', 'Tài chính'),
-    ('SOB', 'Ngân hàng'),
-    ('SOA', 'Kế toán'),
-    ('VDL', 'Du lịch'),
-    ('SOE', 'Kinh tế'),
-    ('SPF', 'Tài chính công'),
-    ('LAW', 'Luật'),
-    ('SOG', 'Quản lý nhà nước'),
-    ('SEM', 'Toán - Thống kê'),
-    ('BIT', 'Công nghệ thông tin kinh doanh'),
-    ('SFL', 'Ngoại ngữ'),
-    ('LLC', 'Lý luận chính trị');
+    ('SOM', N'Quản trị'),
+    ('IBM', N'Kinh doanh quốc tế - Marketing'),
+    ('SOF', N'Tài chính'),
+    ('SOB', N'Ngân hàng'),
+    ('SOA', N'Kế toán'),
+    ('VDL', N'Du lịch'),
+    ('SOE', N'Kinh tế'),
+    ('SPF', N'Tài chính công'),
+    ('LAW', N'Luật'),
+    ('SOG', N'Quản lý nhà nước'),
+    ('SEM', N'Toán - Thống kê'),
+    ('BIT', N'Công nghệ thông tin kinh doanh'),
+    ('SFL', N'Ngoại ngữ'),
+    ('LLC', N'Lý luận chính trị');
 
 CREATE TABLE GIAOVIEN (
     MaGiaoVien varchar(10) PRIMARY KEY,
@@ -24,19 +24,19 @@ CREATE TABLE GIAOVIEN (
     FOREIGN KEY (MaKhoa) REFERENCES KHOA(MaKhoa));
 
 INSERT INTO GIAOVIEN (MaGiaoVien, HoTen, MaKhoa) VALUES
-    ('phungthk',   'Thái Kim Phụng',        'BIT'),
-    ('hienphan',   'Phan Hiền',             'BIT'),
-    ('truong',     'Trương Việt Phương',    'BIT'),
-    ('huybx',      'Bùi Xuân Huy',          'BIT'),
-    ('tuannm',     'Nguyễn Mạnh Tuấn',      'BIT'),
-    ('hvduc',      'Huỳnh Văn Đức',         'BIT'),
-    ('tena',       'Nguyễn An Tế',          'BIT'),
-    ('hungngq',    'Nguyễn Quốc Hùng',      'BIT'),
-    ('khanhntv',   'Ngô Tấn Vũ Khanh',      'BIT'),
-    ('hieubt',     'Bùi Thanh Hiếu',        'BIT'),
-    ('thinhdt',    'Đặng Thái Thịnh',       'BIT'),
-    ('tuyenhtt',   'Hồ Thị Thanh Tuyến',    'BIT'),
-    ('yenhh',      'Hoàng Hải Yến',         'SOB');
+    ('phungthk',   N'Thái Kim Phụng',        'BIT'),
+    ('hienphan',   N'Phan Hiền',             'BIT'),
+    ('truong',     N'Trương Việt Phương',    'BIT'),
+    ('huybx',      N'Bùi Xuân Huy',          'BIT'),
+    ('tuannm',     N'Nguyễn Mạnh Tuấn',      'BIT'),
+    ('hvduc',      N'Huỳnh Văn Đức',         'BIT'),
+    ('tena',       N'Nguyễn An Tế',          'BIT'),
+    ('hungngq',    N'Nguyễn Quốc Hùng',      'BIT'),
+    ('khanhntv',   N'Ngô Tấn Vũ Khanh',      'BIT'),
+    ('hieubt',     N'Bùi Thanh Hiếu',        'BIT'),
+    ('thinhdt',    N'Đặng Thái Thịnh',       'BIT'),
+    ('tuyenhtt',   N'Hồ Thị Thanh Tuyến',    'BIT'),
+    ('yenhh',      N'Hoàng Hải Yến',         'SOB');
 
 
 CREATE TABLE CHUONGTRINH (
@@ -50,14 +50,14 @@ CREATE TABLE CHUONGTRINH (
 
 
 INSERT INTO CHUONGTRINH (MaChuongTrinh, TenChuongTrinh, MaBacHoc, MaKhoa, MaGiaoVien_GiamDocCT) VALUES
-    ('DS_DH', 'Khoa học dữ liệu',                             'DH', 'BIT', 'khanhntv'),
-    ('ERPDH', 'Hệ thống hoạch định nguồn lực doanh nghiệp',   'DH', 'BIT', 'huybx'   ),
-    ('ST_DH', 'Công nghệ Phần mềm',                           'DH', 'BIT', 'truong'  ),
-    ('EC_DH', 'Thương mại điện tử',                           'DH', 'BIT', 'tuannm'  ),
-    ('BISDH', 'Hệ thống thông tin kinh doanh',                'DH', 'BIT', 'hienphan'),
-    ('DT_CH', 'Công nghệ thiết kế thông tin và truyền thông', 'CH', 'BIT', 'phungthk');
-    ('FB_DH', 'Tài chính ngân hàng',                          'CH', 'SOB', 'yenhh'   );
-    ('FB_CH', 'Tài chính ngân hàng',                          'CH', 'SOB', 'yenhh'   );
+    ('DS_DH', N'Khoa học dữ liệu ĐH',                             'DH', 'BIT', 'khanhntv'),
+    ('ERPDH', N'Hệ thống hoạch định nguồn lực doanh nghiệp ĐH',   'DH', 'BIT', 'huybx'   ),
+    ('ST_DH', N'Công nghệ Phần mềm ĐH',                           'DH', 'BIT', 'truong'  ),
+    ('EC_DH', N'Thương mại điện tử ĐH',                           'DH', 'BIT', 'tuannm'  ),
+    ('BISDH', N'Hệ thống thông tin kinh doanh ĐH',                'DH', 'BIT', 'hienphan'),
+    ('DT_CH', N'Công nghệ thiết kế thông tin và truyền thông CH', 'CH', 'BIT', 'phungthk'),
+    ('FB_DH', N'Tài chính ngân hàng ĐH',                          'CH', 'SOB', 'yenhh'   ),
+    ('FB_CH', N'Tài chính ngân hàng CH',                          'CH', 'SOB', 'yenhh'   );
 
 
 CREATE TABLE MONHOC (
@@ -68,53 +68,53 @@ CREATE TABLE MONHOC (
     FOREIGN KEY (MaKhoa) REFERENCES KHOA(MaKhoa));
 
 INSERT INTO MONHOC (MaMonHoc, TenMonHoc, SoTinChi, MaKhoa) VALUES 
-    ('ECO501001', 'Kinh tế vi mô',                                               3,  'SOE'),
-    ('ENG513001', 'Tiếng Anh P1',                                                4,  'BIT'),
-    ('LAW511001', 'Luật kinh doanh',                                             3,  'BIT'),
-    ('MAT508001', 'Toán dành cho kinh tế và quản trị',                           3,  'BIT'),
-    ('PHI510023', 'Triết học Mác LêNin',                                         3,  'BIT'),
-    ('ACC507001', 'Nguyên lý kế toán',                                           3,  'SOA'),
-    ('ECO501002', 'Kinh tế vĩ mô',                                               3,  'SOE'),
-    ('ENG513002', 'Tiếng Anh P2',                                                4,  'SFL'),
-    ('POL510024', 'Kinh tế chính trị Mác LêNin',                                 2,  'LLC'),
-    ('POL510025', 'Chủ nghĩa xã hội khoa học',                                   2,  'LLC'),
-    ('STA508005', 'Thống kê ứng dụng trong Ktế và KD',                           3,  'SEM'),
-    ('ENG513003', 'Tiếng Anh P3',                                                4,  'SFL'),
-    ('HCM510004', 'Tư tưởng Hồ Chí Minh',                                        2,  'LLC'),
-    ('INF509003', 'Cơ sở công nghệ thông tin',                                   3,  'BIT'),
-    ('INF509008', 'Hệ thống thông tin quản lý',                                  3,  'BIT'),
-    ('INF509011', 'Toán dành cho tin học',                                       3,  'BIT'),
-    ('MAN502001', 'Quản trị học',                                                3,  'SOM'),
-    ('ENG513004', 'Tiếng Anh P4',                                                4,  'SFL'),
-    ('HIS510026', 'Lịch sử Đảng Cộng Sản Việt Nam',                              2,  'LLC'),
-    ('INF509004', 'Kiến trúc hệ thống',                                          3,  'BIT'),
-    ('INF509005', 'Cơ sở lập trình',                                             3,  'BIT'),
-    ('INF509006', 'Cơ sở dữ liệu',                                               3,  'BIT'),
-    ('INF509009', 'Phân tích nghiệp vụ kinh doanh',                              3,  'BIT'),
-    ('MAR503001', 'Marketing căn bản',                                           3,  'IBM'),
-    ('MAT508010', 'Phân tích dữ liệu',                                           3,  'SEM'),
-    ('BUS503095', 'Kỹ năng mềm',                                                 2,  'SOM'),
-    ('INF509007', 'Cấu trúc dữ liệu và giải thuật',                              3,  'BIT'),
-    ('INF509010', 'Phân tích thiết kế hệ thống',                                 3,  'BIT'),
-    ('INF509012', 'Công nghệ phần mềm',                                          3,  'BIT'),
-    ('INF509013', 'Thương mại điện tử',                                          3,  'BIT'),
-    ('INF509017', 'Giới thiệu (ERP)',                                            3,  'BIT'),
-    ('ACC507011', 'Kế toán quản trị',                                            3,  'SOA'),
-    ('BUS503048', 'Quản trị chuỗi cung ứng',                                     3,  'SOM'),
-    ('INF509018', 'Cơ sở dữ liệu nâng cao',                                      3,  'BIT'),
-    ('INF509019', 'Đảm bảo và kiểm soát chất lượng phần mềm',                    3,  'BIT'),
-    ('BUS533002', 'Khởi nghiệp kinh doanh',                                      1,  'BIT'),
-    ('INF509014', 'Phát triển ứng dụng web',                                     3,  'BIT'),
-    ('INF509015', 'Phát triển ứng dụng trên Windows',                            3,  'BIT'),
-    ('INF509016', 'Quản lý dự án công nghệ thông tin',                           3,  'BIT'),
-    ('INF509020', 'Tri thức kinh doanh',                                         3,  'BIT'),
-    ('INF509021', 'Kiểm thử phần mềm',                                           3,  'BIT'),
-    ('INF509022', 'Kiến trúc phần mềm hướng dịch vụ',                            3,  'BIT'),
-    ('INF509023', 'Phần mềm hoạch định nguồn lực doanh nghiệp (ERP)',            3,  'BIT'),
-    ('MAN502004', 'Quản trị nguồn nhân lực',                                     3,  'SOM'),
-    ('MAR503018', 'Quản trị marketing',                                          3,  'SOM'),
-    ('INF509033', 'Khóa luận tốt nghiệp - BI',                                   10, 'BIT'),
-    ('INF509072', 'Học kỳ doanh nghiệp - BI',                                    10, 'BIT');
+    ('ECO501001', N'Kinh tế vi mô',                                               3,  'SOE'),
+    ('ENG513001', N'Tiếng Anh P1',                                                4,  'BIT'),
+    ('LAW511001', N'Luật kinh doanh',                                             3,  'BIT'),
+    ('MAT508001', N'Toán dành cho kinh tế và quản trị',                           3,  'BIT'),
+    ('PHI510023', N'Triết học Mác LêNin',                                         3,  'BIT'),
+    ('ACC507001', N'Nguyên lý kế toán',                                           3,  'SOA'),
+    ('ECO501002', N'Kinh tế vĩ mô',                                               3,  'SOE'),
+    ('ENG513002', N'Tiếng Anh P2',                                                4,  'SFL'),
+    ('POL510024', N'Kinh tế chính trị Mác LêNin',                                 2,  'LLC'),
+    ('POL510025', N'Chủ nghĩa xã hội khoa học',                                   2,  'LLC'),
+    ('STA508005', N'Thống kê ứng dụng trong Ktế và KD',                           3,  'SEM'),
+    ('ENG513003', N'Tiếng Anh P3',                                                4,  'SFL'),
+    ('HCM510004', N'Tư tưởng Hồ Chí Minh',                                        2,  'LLC'),
+    ('INF509003', N'Cơ sở công nghệ thông tin',                                   3,  'BIT'),
+    ('INF509008', N'Hệ thống thông tin quản lý',                                  3,  'BIT'),
+    ('INF509011', N'Toán dành cho tin học',                                       3,  'BIT'),
+    ('MAN502001', N'Quản trị học',                                                3,  'SOM'),
+    ('ENG513004', N'Tiếng Anh P4',                                                4,  'SFL'),
+    ('HIS510026', N'Lịch sử Đảng Cộng Sản Việt Nam',                              2,  'LLC'),
+    ('INF509004', N'Kiến trúc hệ thống',                                          3,  'BIT'),
+    ('INF509005', N'Cơ sở lập trình',                                             3,  'BIT'),
+    ('INF509006', N'Cơ sở dữ liệu',                                               3,  'BIT'),
+    ('INF509009', N'Phân tích nghiệp vụ kinh doanh',                              3,  'BIT'),
+    ('MAR503001', N'Marketing căn bản',                                           3,  'IBM'),
+    ('MAT508010', N'Phân tích dữ liệu',                                           3,  'SEM'),
+    ('BUS503095', N'Kỹ năng mềm',                                                 2,  'SOM'),
+    ('INF509007', N'Cấu trúc dữ liệu và giải thuật',                              3,  'BIT'),
+    ('INF509010', N'Phân tích thiết kế hệ thống',                                 3,  'BIT'),
+    ('INF509012', N'Công nghệ phần mềm',                                          3,  'BIT'),
+    ('INF509013', N'Thương mại điện tử',                                          3,  'BIT'),
+    ('INF509017', N'Giới thiệu (ERP)',                                            3,  'BIT'),
+    ('ACC507011', N'Kế toán quản trị',                                            3,  'SOA'),
+    ('BUS503048', N'Quản trị chuỗi cung ứng',                                     3,  'SOM'),
+    ('INF509018', N'Cơ sở dữ liệu nâng cao',                                      3,  'BIT'),
+    ('INF509019', N'Đảm bảo và kiểm soát chất lượng phần mềm',                    3,  'BIT'),
+    ('BUS533002', N'Khởi nghiệp kinh doanh',                                      1,  'BIT'),
+    ('INF509014', N'Phát triển ứng dụng web',                                     3,  'BIT'),
+    ('INF509015', N'Phát triển ứng dụng trên Windows',                            3,  'BIT'),
+    ('INF509016', N'Quản lý dự án công nghệ thông tin',                           3,  'BIT'),
+    ('INF509020', N'Tri thức kinh doanh',                                         3,  'BIT'),
+    ('INF509021', N'Kiểm thử phần mềm',                                           3,  'BIT'),
+    ('INF509022', N'Kiến trúc phần mềm hướng dịch vụ',                            3,  'BIT'),
+    ('INF509023', N'Phần mềm hoạch định nguồn lực doanh nghiệp (ERP)',            3,  'BIT'),
+    ('MAN502004', N'Quản trị nguồn nhân lực',                                     3,  'SOM'),
+    ('MAR503018', N'Quản trị marketing',                                          3,  'SOM'),
+    ('INF509033', N'Khóa luận tốt nghiệp - BI',                                   10, 'BIT'),
+    ('INF509072', N'Học kỳ doanh nghiệp - BI',                                    10, 'BIT');
 
 CREATE TABLE CHUONGTRINHMONHOC (
   MaChuongTrinh nvarchar(5) NOT NULL,
@@ -186,6 +186,23 @@ CREATE TABLE DAMNHIEMMON (
     FOREIGN KEY (MaChuongTrinh) REFERENCES CHUONGTRINH(MaChuongTrinh),
     FOREIGN KEY (MaMonHoc) REFERENCES MONHOC(MaMonHoc),
     FOREIGN KEY (MaGiaoVien) REFERENCES GIAOVIEN(MaGiaoVien));
+GO
+Create trigger check_dam_nhiem_mon
+On DamNhiemMon
+FOR INSERT, UPDATE
+AS 
+BEGIN
+	DECLARE @magiaovien varchar(10), @coladamnhiemchinh int, @count int = 0
+	SELECT @magiaovien = MaGiaoVien FROM inserted
+	SELECT @coladamnhiemchinh = CoLaDamNhiemChinh FROM inserted
+	SELECT 	@count = COUNT (DISTINCT MaMonHoc)  FROM DamNhiemMon WHERE MaGiaoVien = @magiaovien AND CoLaDamNhiemChinh = 1
+	If (@count > 5 AND @coladamnhiemchinh = 1)
+	begin
+		ROLLBACK TRAN
+	end
+END
+GO
+
 
 INSERT INTO DAMNHIEMMON (MaChuongTrinh, MaMonHoc, MaGiaoVien, CoLaDamNhiemChinh) VALUES 
     ('BISDH', 'INF509007', 'hvduc',    0),
@@ -213,11 +230,11 @@ CREATE TABLE KHOAHOC (
     FOREIGN KEY (MaChuongTrinh) REFERENCES CHUONGTRINH(MaChuongTrinh));
 
 INSERT INTO KHOAHOC (MaKhoaHoc, TenKhoaHoc, NamBatDau, NamKetThuc, MaChuongTrinh) VALUES
-    ('DH46BI', 'Hệ thống thông tin kinh doanh Khóa 46',                2020, 2024, 'BISDH'),
-    ('DH47BI', 'Hệ thống thông tin kinh doanh Khóa 47',                2021, 2025, 'BISDH'),
-    ('DH46EC', 'Thương mại điện tử Khóa 46',                           2020, 2024, 'EC_DH'),
-    ('DH46ST', 'Công nghệ Phần mềm Khóa 46',                           2020, 2024, 'ST_DH'),
-    ('CH32DT', 'CN thiết kế thông tin và truyền thông Khóa 32',        2022, 2024, 'DT_CH');
+    ('DH46BI', N'Hệ thống thông tin kinh doanh Khóa 46',                2020, 2024, 'BISDH'),
+    ('DH47BI', N'Hệ thống thông tin kinh doanh Khóa 47',                2021, 2025, 'BISDH'),
+    ('DH46EC', N'Thương mại điện tử Khóa 46',                           2020, 2024, 'EC_DH'),
+    ('DH46ST', N'Công nghệ Phần mềm Khóa 46',                           2020, 2024, 'ST_DH'),
+    ('CH32DT', N'CN thiết kế thông tin và truyền thông Khóa 32',        2022, 2024, 'DT_CH');
 
 CREATE TABLE KHOAHOCMON (
     MaKhoaHoc nvarchar(10) NOT NULL,
